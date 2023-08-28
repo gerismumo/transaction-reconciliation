eade18165460b247a57778ee0a53240d63b5e19c
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-08-28 16:32:02 +0300

    build folder

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,13 @@ const dotenv = require('dotenv');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
