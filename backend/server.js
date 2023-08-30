dbb0dae59c2a5b3e35dd53ce1fa8b57703319e25
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-08-30 14:56:46 +0300

    use proxy url1

--- backend/server.js
+++ backend/server.js
@@ -19,18 +19,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/transactions', require('./routes/transactionRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) =>
+app.get('/', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      path.resolve(__dirname, 'index.html')
     )
   );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
+// Serve frontend
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+//   app.get('*', (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+//     )
+//   );
+// } else {
+//   app.get('/', (req, res) => res.send('Please set to production'));
+// }
+
 
 app.use(errorHandler);
 
