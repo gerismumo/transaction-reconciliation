b2b601bd9ab56e64a4a994048824e07dee9b98d8
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-08-31 10:18:44 +0300

    deployment changes11

--- server.js
+++ server.js
@@ -1,31 +1,12 @@
-const path = require('path');
 const express = require('express');
-const colors = require('colors');
 const dotenv = require('dotenv');
 const { errorHandler } = require('./backend/middleware/errorMiddleware');
 const connectDB = require('./backend/config/db');
-const port = process.env.PORT || 5000;
-// const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
-// const allowedOrigins = ['https://transaction-reconciliation.vercel.app', 'http://localhost'];
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.includes(origin) || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
-
-// Use CORS middleware with options
-// app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,26 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/transactions', require('./backend/routes/transactionRoutes'));
 app.use('/api/users', require('./backend/routes/userRoutes'));
 
-
-// Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static(path.join(__dirname, '../frontend/build/static')));
-
-  // app.get('/', (req, res) =>
-  //   res.sendFile(
-  //     path.resolve(__dirname, '../', 'frontend', 'build', 'static', 'index.html')
-  //   )
-  // );
-  app.get('/', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
-
-
 app.use(errorHandler);
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
