65fb2d996eadde16224672096fa38dfdd71bf07f
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-08-31 09:33:43 +0300

    deployment changes3

--- backend/server.js
+++ backend/server.js
@@ -40,11 +40,11 @@ app.get('/', (req, res) =>
   );
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      path.resolve(__dirname, '../', 'frontend', 'build', 'static', 'index.html')
     )
   );
 } else {
