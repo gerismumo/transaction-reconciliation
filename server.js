ce621449362349b74999824cb0f22f242ff08ca0
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-08-31 10:01:36 +0300

    deployment changes9

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv');
-const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const { errorHandler } = require('./backend/middleware/errorMiddleware');
+const connectDB = require('./backend/config/db');
 const port = process.env.PORT || 5000;
 // const cors = require('cors');
 
@@ -30,8 +30,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/transactions', require('./routes/transactionRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/transactions', require('./backend/routes/transactionRoutes'));
+app.use('/api/users', require('./backend/routes/userRoutes'));
 
 
 // Serve frontend
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === 'production') {
   // );
   app.get('/', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, 'backend', 'index.html')
+      path.resolve(__dirname, 'index.html')
     )
   );
 } else {
