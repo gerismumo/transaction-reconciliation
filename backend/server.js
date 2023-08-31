968c26aa2195cd41cb163aa4906b6dcd4c2fbd1b
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-08-31 09:53:29 +0300

    deployment changes6

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use('/api/users', require('./routes/userRoutes'));
 //   );
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build/static')));
+  // app.use(express.static(path.join(__dirname, '../frontend/build/static')));
 
   app.get('/', (req, res) =>
     res.sendFile(
