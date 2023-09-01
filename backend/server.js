ff3a9e82b7a237b2978aa4829cef1d882994944c
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-09-01 10:03:38 +0300

    test11

--- backend/server.js
+++ backend/server.js
@@ -6,48 +6,29 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-// import cookieParser from "cookie-parser";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-// const allowedOrigins = ['https://transaction-reconciliation.vercel.app', 'http://localhost'];
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (allowedOrigins.includes(origin) || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-// };
-
-// Use CORS middleware with options
-// app.use(cors(corsOptions));
 app.use(cors());
-// app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/transactions', require('./routes/transactionRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
-
-// Serve frontend
+// Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
-  
-  app.get('/', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
+  // Serve static files from the "public" directory
+  app.use(express.static(path.join(__dirname, 'public')));
 
+  // Catch-all route for any other requests, serving the main HTML file
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+}
 
 app.use(errorHandler);
 
