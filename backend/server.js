04cfab3135de768d06a0dbe611c24bd0bfcd5ea8
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-09-01 11:12:53 +0300

    vercel2

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 const colors = require('colors');
+const cookieParser = require("cookie-parser");
+const http = require("http");
 const dotenv = require('dotenv');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-// import cookieParser from "cookie-parser";
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/transactions', require('./routes/transactionRoutes'));
@@ -33,7 +35,8 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => res.send('Please set to production'));
 }
 
-
 app.use(errorHandler);
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
