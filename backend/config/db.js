a4f3d42151a7e70b8d48a8389bdaec336a0984ff
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-09-01 14:09:34 +0300

    no retry interval

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,35 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  const mongoDBUrl = process.env.MONGO_URI;
-  const maxRetries = 5; // Maximum number of connection retries
-  const retryInterval = 20000; // Retry interval in milliseconds (e.g., 5 seconds)
-  let retryCount = 0;
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI)
 
-  const connectWithRetry = async () => {
-    try {
-      const conn = await mongoose.connect(mongoDBUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
 
-      console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-    } catch (error) {
-      console.error('MongoDB Connection Error:', error);
-
-      if (retryCount < maxRetries) {
-        retryCount++;
-        console.log(`Retrying connection in ${retryInterval / 1000} seconds...`);
-        setTimeout(connectWithRetry, retryInterval);
-      } else {
-        console.error('Max connection retries reached. Exiting...');
-        process.exit(1);
-      }
-    }
-  };
-
-  // Call the function to initiate the connection with retry logic
-  connectWithRetry();
-};
-
-module.exports = connectDB;
+module.exports = connectDB
