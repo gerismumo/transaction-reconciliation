ff7de834686aa2371d1132eb12e097f61a4191cb
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-09-01 12:33:50 +0300

    config1

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
-const path = require('path');
 const express = require('express');
-const colors = require('colors');
-const cookieParser = require("cookie-parser");
-const http = require("http");
+const cors = require('cors');
 const dotenv = require('dotenv');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
-const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cors(
   {
@@ -21,9 +20,6 @@ app.use(cors(
   }
 ));
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/transactions', require('./routes/transactionRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
@@ -33,7 +29,5 @@ app.get("/", (req, res) => {
 })
 
 app.use(errorHandler);
-const server = http.createServer(app);
-server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
