6c19966d56d1dcc83542daf4527d95c358481521
gerismumo <geraldmumo6@gmail.com>
2023-09-20 15:24:38 +0300

    created branch

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors(
   {
    
-      origin: ["https://transaction-reconciliation.vercel.app"],
+      origin: ["http://localhost:3000"],
       methods: ["POST", "GET", "PUT", "DELETE"]
   }
 ));
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
 import { logout, reset } from '../features/auth/authSlice';
 
 function Header() {
