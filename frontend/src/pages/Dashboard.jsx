67541fca798b895b220256876f7b5c03deac5aae
gerismumo <geraldmumo6@gmail.com>
2023-09-20 16:11:02 +0300

    change

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div className="dashboard">
+            <h1>Dashboard page</h1>
+            <p>Details</p>
+        </div>
+    )
+}
+
+export default Dashboard;
